3ae0ffe0e9f980e7cc5d1a57db0a6e17112becb7
Jon Alas <jalas121@gmail.com>
2021-11-29 09:00:52 -0600

    third commit

--- 01-Cafe_Repository/Menu.cs
+++ 01-Cafe_Repository/Menu.cs
@@ -8,14 +8,14 @@ namespace _01_Restaurant_Repository
 {
     public class Menu
     {
-        public int MealNumber { get; set; }
-        public string MealName { get; set; }
-        public string Description { get; set; }
-        public List<string> Ingredients { get; set; }
-        public double Price { get; set; }
+        //public int MealNumber { get; set; }
+        //public string MealName { get; set; }
+        //public string Description { get; set; }
+        //public List<string> Ingredients { get; set; }
+       //public double Price { get; set; }
 
-        public Menu() { }
-        public Menu(int mealNumber, string mealName, string mealDescription, List<string> mealIngredients, double mealPrice)
+        //public Menu() { }
+        //public Menu(int mealNumber, string mealName, string mealDescription, List<string> mealIngredients, double mealPrice)
         {
             //MealNumber = mealNumber;
            // MealName = mealName;
